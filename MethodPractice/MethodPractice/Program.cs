1ab10a4772643a7b7ab0deb2110418862a3690e0
coreyareid <coreyareid@bellsouth.net>
2022-07-05 10:16:03 -0500

    Fixed some typos.

--- MethodPractice/MethodPractice/Program.cs
+++ MethodPractice/MethodPractice/Program.cs
@@ -1,22 +1,22 @@
 ﻿// Exercise 1
+/*
+Console.WriteLine("What is your name?");
+var yourName = Console.ReadLine();
 
-    Console.WriteLine("What is your name?");
-    var yourName = Console.ReadLine();
+Console.WriteLine("What is your favorite color?");
+var favoriteColor = Console.ReadLine();
 
-    Console.WriteLine("What is your favorite color?");
-    var favoriteColor = Console.ReadLine();
+Console.WriteLine("What is your favorite animal?");
+var favoriteAnimal = Console.ReadLine();
 
-    Console.WriteLine("What is your favorite animal?");
-    var favoriteAnimal = Console.ReadLine();
-
-    Console.WriteLine("What is your favorite band?");
-    var band = Console.ReadLine();
+Console.WriteLine("What is your favorite band?");
+var band = Console.ReadLine();
 
 
-    Console.WriteLine($" Hello, {yourName} and welcome to this exercise. I see you like " +
-        $"the color {favoriteColor}, maybe if you had a {favoriteColor} " +
-        $"{favoriteAnimal} you wouldn\'t feel lonely " +
-        $"and need to listen to {band} to cope with your sadness.");
+Console.WriteLine($" Hello, {yourName} and welcome to this exercise. I see you like " +
+    $"the color {favoriteColor}, maybe if you had a {favoriteColor} " +
+    $"{favoriteAnimal} you wouldn\'t feel lonely " +
+    $"and need to listen to {band} to cope with your sadness.");
 // Exercise 2
 
 static int Addition(int x, int y)
@@ -31,7 +31,7 @@ int answer = Addition(0, 0);
 Console.WriteLine($"your result is: {answer}");
 
 
-static int Multiplication( int numOne, int numTwo)
+static int Multiplication(int numOne, int numTwo)
 {
     Console.WriteLine("Put in two numbers you want to be multiplied.");
     numOne = Convert.ToInt32(Console.ReadLine());
@@ -42,7 +42,7 @@ static int Multiplication( int numOne, int numTwo)
 int answerTwo = Multiplication(0, 0);
 Console.WriteLine($"You multiplied two numbers to get: {answerTwo}");
 
-static int Subtraction( int thingOne, int thingTwo)
+static int Subtraction(int thingOne, int thingTwo)
 {
     Console.WriteLine("Put in two numbers to subtract.");
     thingOne = Convert.ToInt32(Console.ReadLine());
@@ -52,12 +52,13 @@ static int Subtraction( int thingOne, int thingTwo)
 
 int answerThree = Subtraction(0, 0);
 Console.WriteLine($"Your answer for subtractions is: {answerThree}");
-
+*/
 static int AddSeveralNumbers(params int[] numbers)
 {
     int total = 0;
     foreach (int number in numbers)
-    { total += number;
+    {
+        total += number;
     }
     return total;
 }
@@ -66,6 +67,6 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 int num2 = Convert.ToInt32(Console.ReadLine());
 int num3 = Convert.ToInt32(Console.ReadLine());
 int num4 = Convert.ToInt32(Console.ReadLine());
-int[] numbersInMethod = { num1, num2, num3, num4 };
-int addedNumbers = AddSeveralNumbers(numbersInMethod);
-Console.WriteLine($"Youre total is: {addedNumbers}.");
+//int[] numbersInMethod = { num1, num2, num3, num4 };
+int addedNumbers = AddSeveralNumbers(num1, num2, num3, num4);
+Console.WriteLine($"Your total is: {addedNumbers}.");
